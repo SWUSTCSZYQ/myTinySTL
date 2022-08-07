1147469a4305760129b346bdefb255641247d6db
SWUSTCSZYQ <1697782579@qq.com>
2022-08-07 17:08:58 +0800

    新增一些简单算法

--- algorithm/algorithm.h
+++ algorithm/algorithm.h
@@ -5,4 +5,73 @@
 #ifndef MYTINYSTL_ALGORITHM_H
 #define MYTINYSTL_ALGORITHM_H
 
+#include "../iterator/iterator.h"
+
+///数值算法
+
+///求和
+template<typename InputIterator, typename T>
+T accumulate(InputIterator first, InputIterator last, T init){
+    while(first++ != last)
+    {
+        init += *first;
+    }
+    return init;
+}
+
+template<typename InputIterator, typename T, typename BinaryOperator>
+T accumulate(InputIterator first, InputIterator last, T init, BinaryOperator op)
+{
+    while(first++ != last)
+        init += op(init, *first);
+    return init;
+}
+
+///计算 first 和 last 中相邻元素的差额
+template <typename InputIterator, typename OutputIterator, typename T>
+OutputIterator __adjacent_difference(InputIterator first, InputIterator last, OutputIterator result, T*){
+    T value = *first;
+    while(++first != last){
+        T tmp = *first;
+        *++result = tmp - value;
+        value = tmp;
+    }
+    return ++result;
+}
+
+template <typename InputIterator, typename OutputIterator>
+OutputIterator adjacent_difference(InputIterator first, InputIterator last, OutputIterator result)
+{
+    if(first == last)return result;
+    return __adjacent_difference(first, last, result, value_type(first));
+}
+
+template <typename InputIterator, typename OutputIterator, typename T, typename BinaryOperation>
+OutputIterator __adjacent_difference(InputIterator first, InputIterator last, OutputIterator result, T*, BinaryOperation op){
+    T value = *first;
+    while(++first != last){
+        T tmp = *first;
+        *++result = op(tmp, value);
+        value = tmp;
+    }
+    return ++result;
+}
+
+template <typename InputIterator, typename OutputIterator, typename BinaryOperation>
+OutputIterator adjacent_difference(InputIterator first, InputIterator last, OutputIterator result, BinaryOperation op)
+{
+    if(first == last)return result;
+    *result = first;
+    return __adjacent_difference(first, last, result, value_type(first), op);
+}
+
+
+template<typename ForwardIterator, typename T>
+void iota(ForwardIterator first, ForwardIterator last, T value)
+{
+    while(first != last) *first++ = value++;
+}
+
+
+
 #endif //MYTINYSTL_ALGORITHM_H
---
+++ container/rbTree.h
@@ -0,0 +1,8 @@
+//
+// Created by Lenovo on 2022/8/7.
+//
+
+#ifndef MYTINYSTL_RBTREE_H
+#define MYTINYSTL_RBTREE_H
+
+#endif //MYTINYSTL_RBTREE_H
